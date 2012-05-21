b1fba5b46d0b4750fcb54eb9b216daf4c91b052a
Arthur Street <arthurstreet@yahoo.com>
2012-05-21 12:24:31 +1000

    testing - a new single view template

---
+++ Pears/Pears/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pears
+//
+//  Created by Arthur Street on 21/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pears/PearsTests/PearsTests.m
@@ -0,0 +1,32 @@
+//
+//  PearsTests.m
+//  PearsTests
+//
+//  Created by Arthur Street on 21/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PearsTests.h"
+
+@implementation PearsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PearsTests");
+}
+
+@end
